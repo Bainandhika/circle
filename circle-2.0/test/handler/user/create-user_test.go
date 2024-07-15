0ea8d8ccf3fbcc6d402be0c60f7d68c0eebe1cea
Admin Dans <bainandhika.baghaskara@dansmultipro.com>
2024-07-15 13:11:36 +0700

    create create-user test

--- circle-2.0/test/handler/user/create-user_test.go
+++ circle-2.0/test/handler/user/create-user_test.go
@@ -7,8 +7,10 @@ import (
 	"net/http/httptest"
 	"testing"
 
+	"circle-2.0/app/config"
 	"circle-2.0/app/pkg/repository/user"
 	"circle-2.0/lib/connection/database"
+	"circle-2.0/lib/connection/nosql"
 	"circle-2.0/lib/helper/constant"
 	"circle-2.0/lib/model"
 	"circle-2.0/test"
@@ -17,6 +19,12 @@ import (
 
 func TestCreateUser(t *testing.T) {
 	app := test.SetUpTestApp()
+	defer func ()  {
+		db, _ := database.MySQLConnect.DB()
+		db.Close()
+
+		nosql.RedisConnect.Close()
+	}()
 
 	requestBody := model.CreateUserRequest{
 		Name:     "John Doe",
@@ -26,47 +34,53 @@ func TestCreateUser(t *testing.T) {
 
 	requestBodyBytes, err := app.Config().JSONEncoder(requestBody)
 	if err != nil {
-		assert.Nil(t, err)
+		assert.Nil(t, err, err.Error())
+		return
 	}
 
 	req := httptest.NewRequest(http.MethodPost, constant.CreateUserPath, bytes.NewReader(requestBodyBytes))
 	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("secret-key", config.App.SecretKey)
+	req.Header.Set("channel-id", "test-channel")
 
 	resp, err := app.Test(req, -1)
-	assert.Nil(t, err)
+	if err != nil {
+		assert.Nil(t, err)
+		return
+	}
+
 	assert.Equal(t, http.StatusOK, resp.StatusCode)
 
 	respBody, err := io.ReadAll(resp.Body)
 	if err != nil {
-		assert.Nil(t, err)
+		assert.Nil(t, err, err.Error())
+		return
 	}
 
 	var responseData model.Response
 	if err = app.Config().JSONDecoder(respBody, &responseData); err != nil {
-		assert.Nil(t, err)
-	}
-
-	actualUserData, ok := responseData.Data.(model.Users)
-	if !ok {
-		assert.Equal(t, true, ok)
+		assert.Nil(t, err, err.Error())
+		return
 	}
 
 	u := user.NewUserRepo(database.MySQLConnect)
-	
+
 	userData, err := u.GetUserByEmail(requestBody.Email)
 	if err != nil {
-		assert.Nil(t, err)
-    }
+		assert.Nil(t, err, err.Error())
+		return
+	}
 
 	if err = u.DeleteUser(userData.ID); err != nil {
-		assert.Nil(t, err)
-    }
+		assert.Nil(t, err, err.Error())
+		return
+	}
 
-	assert.Equal(t, requestBody.Name, actualUserData.Name)
-	assert.Equal(t, requestBody.Email, actualUserData.Email)
-	assert.Equal(t, requestBody.Password, actualUserData.Password)
-	assert.NotEmpty(t, actualUserData.ID)
-	assert.NotEmpty(t, actualUserData.CreatedAt)
-	assert.NotEmpty(t, actualUserData.UpdatedAt)
-	assert.Empty(t, actualUserData.UpdatedBy)
+	assert.Equal(t, requestBody.Name, userData.Name)
+	assert.Equal(t, requestBody.Email, userData.Email)
+	assert.NotEmpty(t, userData.Password)
+	assert.NotEmpty(t, userData.ID)
+	assert.NotEmpty(t, userData.CreatedAt)
+	assert.NotEmpty(t, userData.UpdatedAt)
+	assert.NotEmpty(t, userData.UpdatedBy)
 }
